dd70c4224173ba2f719ff9ac16edde78466533e5
jaidenmeiden <jaidenmeiden@gmail.com>
2023-09-14 13:16:17 +0200

    Add new controller proof

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ProofController} from "./proof/proof.controller";
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [
+    AppController,
+    ProofController
+  ],
   providers: [AppService],
 })
 export class AppModule {}
