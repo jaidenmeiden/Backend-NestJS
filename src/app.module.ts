ba884ef3ecbce71434c4179236fe8017107b5bf8
jaidenmeiden <jaidenmeiden@gmail.com>
2023-09-17 01:34:45 +0200

    Configure and implement useFactory with HttpModule (Include in Nest.js). Remember that it is an asynchronous module (External API is an example)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProofController } from "./proof/proof.controller";
@@ -9,7 +11,7 @@ const API_KEY = process.env.API_KEY;
 const API_KEY_PROD = process.env.API_KEY_PROD;
 
 @Module({
-  imports: [ProductsModule, UsersModule],
+  imports: [HttpModule, ProductsModule, UsersModule],
   controllers: [
     AppController,
     ProofController
@@ -19,6 +21,15 @@ const API_KEY_PROD = process.env.API_KEY_PROD;
     {
       provide: 'API_KEY',
       useValue: process.env.APP_ENV === 'production' ? API_KEY_PROD : API_KEY,
+    },
+    {
+      provide: 'DATA',
+      useFactory: async (http: HttpService) => {
+        const request = http.get('https://jsonplaceholder.typicode.com/todos');
+        const tasks = await lastValueFrom(request);
+        return tasks.data;
+      },
+      inject: [HttpService],
     },
   ],
 })
