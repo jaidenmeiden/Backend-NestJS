2878869f0f8945122afe415147e7ea2f5e2eb147
jaidenmeiden <jaidenmeiden@gmail.com>
2023-09-17 01:52:57 +0200

    Inject global module database in user services

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, Inject } from '@nestjs/common';
 
 import { User } from '../entities/user.entity';
 import { CreateUserDto, UpdateUserDto } from '../dtos/user.dto';
@@ -18,7 +18,10 @@ export class UsersService {
     },
   ];
 
-  constructor(private productsService: ProductsService) {}
+  constructor(
+    private productsService: ProductsService,
+    @Inject('API_KEY') private apiKey: string,
+  ) {}
 
   findAll() {
     return this.users;
