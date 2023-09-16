941aa499ad66863cf259c257d8dee076c1ebf9e3
jaidenmeiden <jaidenmeiden@gmail.com>
2023-09-17 00:15:25 +0200

    Import module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ProductsModule } from '../products/products.module';
 import { CustomerController } from "./controllers/customers.controller";
 import { UsersController } from "./controllers/users.controller";
 import { CustomersService } from "./services/customers.service";
 import { UsersService } from "./services/users.service";
 
 @Module({
+  imports: [
+    ProductsModule
+  ],
   controllers: [
     CustomerController,
     UsersController,
