5edb57ab0e9f0987411427ea9a3706b45d3f108e
jaidenmeiden <jaidenmeiden@gmail.com>
2023-09-16 17:28:03 +0200

    Avoid whitelist and forbidNonWhitelisted

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,                    // Ignore data not included in DTO
+      forbidNonWhitelisted: true,         // Throw error if prohibited data exists
+      //disableErrorMessages: true,         // Disable error messages (production)
+    })
+  );
   await app.listen(8080);
 }
 bootstrap();
