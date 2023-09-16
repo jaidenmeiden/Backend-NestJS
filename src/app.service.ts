c6dc18079b13c7a7c9e8c512a2802d18e8b94b77
jaidenmeiden <jaidenmeiden@gmail.com>
2023-09-17 01:35:12 +0200

    Inject useFactory variable into service

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,9 +2,13 @@ import { Injectable, Inject } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  constructor(@Inject('API_KEY') private apiKey: string) {}
+  constructor(
+    @Inject('API_KEY') private apiKey: string,
+    @Inject('DATA') private tasks: any[]
+  ) {}
 
   getHello(): string {
+    console.log(this.tasks);
     return `Hello World with ${this.apiKey}`;
   }
 }
