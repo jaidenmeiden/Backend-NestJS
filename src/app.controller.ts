427535f8dd5961c9cca396ac30873106adb11559
jaidenmeiden <jaidenmeiden@gmail.com>
2023-09-17 01:40:06 +0200

    Inject useFactory variable into controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('DATA') private data: any[]
+  ) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello(JSON.stringify(this.data[0]));
   }
 }
