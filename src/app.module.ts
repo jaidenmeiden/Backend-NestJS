09d60d8f3225f6f5312f27851e633dfb246985d2
jaidenmeiden <jaidenmeiden@gmail.com>
2023-09-16 23:16:15 +0200

    Import new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,35 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProofController } from "./proof/proof.controller";
-import { CategoriesController } from './controllers/categories.controller';
-import { ProductsController } from './controllers/products.controller';
-import { BrandsController } from './controllers/brands.controller';
-import { CustomerController } from './controllers/customers.controller';
-import { UsersController } from './controllers/users.controller';
-import { CategoriesService } from './services/categories.service';
-import { ProductsService } from './services/products.service';
-import { BrandsService } from './services/brands.service';
-import { CustomersService } from './services/customers.service';
-import { UsersService } from './services/users.service';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, UsersModule],
   controllers: [
     AppController,
-    ProofController,
-    ProductsController,
-    CategoriesController,
-    BrandsController,
-    CustomerController,
-    UsersController,
+    ProofController
   ],
   providers: [
-    AppService,
-    ProductsService,
-    CategoriesService,
-    BrandsService,
-    CustomersService,
-    UsersService,
+    AppService
   ],
 })
 export class AppModule {}
