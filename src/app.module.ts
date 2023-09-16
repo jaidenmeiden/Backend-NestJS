65dafb473ccad4af747b7adceb4a750a1fc948be
jaidenmeiden <jaidenmeiden@gmail.com>
2023-09-17 01:10:09 +0200

    Inject value (API_KEY) from .env with useValue

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { ProofController } from "./proof/proof.controller";
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 
+const API_KEY = process.env.API_KEY;
+const API_KEY_PROD = process.env.API_KEY_PROD;
+
 @Module({
   imports: [ProductsModule, UsersModule],
   controllers: [
@@ -12,7 +15,11 @@ import { UsersModule } from './users/users.module';
     ProofController
   ],
   providers: [
-    AppService
+    AppService, //useClass
+    {
+      provide: 'API_KEY',
+      useValue: process.env.APP_ENV === 'production' ? API_KEY_PROD : API_KEY,
+    },
   ],
 })
 export class AppModule {}
