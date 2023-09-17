935103a15d9f18a259eaa2c4a11e6d77039189e8
jaidenmeiden <jaidenmeiden@gmail.com>
2023-09-17 14:26:16 +0200

    Use config file to call variables

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,20 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigType } from '@nestjs/config';
+import config from './config';
 
 @Injectable()
 export class AppService {
   constructor(
-    private config: ConfigService,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
     @Inject('DATA') private tasks: any[]
   ) {};
 
   getHello(value: string): string {
     //console.log(this.tasks);
-    const apiKey = this.config.get<string>('API_KEY');
-    const domain = this.config.get('APP_DOMAIN');
-    return `Hello World!  Envs: ${apiKey} and  ${domain} con ConfigService. From controller: ${value}`;
+    const apiKey = this.configService.apiKey;
+    const host = this.configService.database.host;
+    const port = this.configService.database.port;
+    const domain = this.configService.application.domain;
+    return `Hello World!  Envs: <strong>${apiKey}</strong> and  <strong>${host}:${port}</strong> into <strong>${domain}</strong> con ConfigService. From controller: <strong>${value}</strong>`;
   }
 }
