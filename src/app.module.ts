2c75bdeb0a2227b47b40cdbb164975adb412031a
jaidenmeiden <jaidenmeiden@gmail.com>
2023-09-17 15:47:58 +0200

    Apply validation schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import config from './config';
 import { HttpModule, HttpService } from '@nestjs/axios';
 import { lastValueFrom } from 'rxjs'
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProofController } from "./proof/proof.controller";
@@ -10,13 +11,20 @@ import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { environment } from './environment';
+import config from './config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: environment[process.env.APP_ENV] || '.env.local',
+      envFilePath: environment[process.env.START_APP_ENV] || '.env.local',
       load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.string().required(),
+        DB_CONNECTION: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+      })
     }),
     HttpModule,
     ProductsModule,
