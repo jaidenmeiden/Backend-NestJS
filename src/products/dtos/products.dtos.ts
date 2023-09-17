4d2bddb147e4ccd19b2a00f79fe75742a47269ea
jaidenmeiden <jaidenmeiden@gmail.com>
2023-09-17 16:22:12 +0200

    Apply documentation into DTOs

--- src/products/dtos/products.dtos.ts
+++ src/products/dtos/products.dtos.ts
@@ -1,27 +1,35 @@
 import { IsNotEmpty, IsString, IsNumber, IsPositive, IsUrl } from 'class-validator';
-import { PartialType, OmitType } from '@nestjs/mapped-types';
+import { ApiProperty, PartialType, OmitType } from '@nestjs/swagger';
+
 export class CreateProductDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   readonly name: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   readonly description: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsNumber()
   @IsPositive()
   readonly price: number;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsNumber()
   @IsPositive()
   readonly stock: number;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsUrl()
   readonly image: string;
 }
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(
+  OmitType(CreateProductDto, ['name']),
+) {}
