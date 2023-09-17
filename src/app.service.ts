94c5214066ff89805e66781260544e356e473a0b
jaidenmeiden <jaidenmeiden@gmail.com>
2023-09-17 14:01:35 +0200

    Adjust app services

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,9 +3,13 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor(private config: ConfigService) {}
+  constructor(
+    private config: ConfigService,
+    @Inject('DATA') private tasks: any[]
+  ) {};
 
   getHello(value: string): string {
+    //console.log(this.tasks);
     const apiKey = this.config.get<string>('API_KEY');
     const domain = this.config.get('APP_DOMAIN');
     return `Hello World!  Envs: ${apiKey} and  ${domain} con ConfigService. From controller: ${value}`;
